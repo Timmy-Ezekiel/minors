ebdd372437933bb01ab536b0bb069dc1ab571d50
Timmy-Ezekiel <timilehinoluwatuyi98@gmail.com>
2022-09-18 14:19:10 +0000

    Rectangle Creator

---
+++ rectangle.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+/**
+  * myRectangle - This function create a rectangle
+  *
+  * @height : This is an interger parameter for the rectangle height.
+  * @width: this is an integer paramet for the rectangle width.
+  *
+ **/
+
+void myRectangle(int height, int width)
+{
+	#define INITIAL 1
+
+	int x, y;
+	int Nwidth = width * 2;
+
+	for (x = INITIAL; x <= height; x++)
+	{
+		if (x == 1)
+		{
+			for (y = INITIAL; y <= Nwidth; y++)
+			{
+				putchar('_');
+			}
+		}
+		else if (x == height)
+		{
+			for (y = INITIAL; y <= Nwidth; y++)
+			{
+				if ((y == INITIAL) || (y == Nwidth))
+				{
+					putchar('|');
+				}
+				else
+				{
+					putchar('_');
+				}
+			}
+		}
+		else
+		{
+			for (y = INITIAL; y <= Nwidth; y++)
+			{
+				if ((y != INITIAL) && (y != Nwidth))
+				{
+					putchar(' ');
+				}
+				else
+				{
+					putchar('|');
+				}
+			}
+		}
+		putchar('\n');
+	}
+return;
+}
